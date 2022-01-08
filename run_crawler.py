b58b55f80dcba456225b2a36ac69746c64149c6d
Leon Blumenthal <dev.blumenthal.leon@gmail.com>
2022-01-08 01:25:25 +0100

    added crawler run script including env file

---
+++ run_crawler.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+
+from crawler import Crawler
+
+
+def run():
+    """Run crawler"""
+
+    load_dotenv()
+
+    url = os.getenv('URL', 'https://www.ub.tum.de/en/reserve-study-desks')
+    data_path = os.getenv('DATA_PATH', 'data')
+
+    crawler = Crawler(url, data_path)
+
+    crawler.run()
+
+
+if __name__ == '__main__':
+    run()
